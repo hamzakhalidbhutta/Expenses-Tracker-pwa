40e2a2df1d088d501b6bff2344218920c5a4623c
Hamza Khalid Bhutta <hamzakhalidbhutta430@gmail.com>
2022-02-09 18:03:34 +0500

    Made Responsive

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -3,25 +3,28 @@ import React from "react";
 const Card = (props: any): JSX.Element => {
   return (
     <>
-      <button
-        className="spanUpdate"
-        onClick={(e) => {
-          e.preventDefault();
-          props.updateTransection(props.id);
-        }}
-      >
-        Edit
-      </button>
-      <button
-        className="spanDel"
-        onClick={(e) => {
-          e.preventDefault();
-          props.delTransection(props.id);
-        }}
-      >
-        Delete
-      </button>
       <div className="card">
+        <div className="controls">
+          {" "}
+          <button
+            className="spanDel"
+            onClick={(e) => {
+              e.preventDefault();
+              props.delTransection(props.id);
+            }}
+          >
+            X
+          </button>
+          <button
+            className="spanUpdate"
+            onClick={(e) => {
+              e.preventDefault();
+              props.updateTransection(props.id);
+            }}
+          >
+            !
+          </button>
+        </div>
         <div className="title">{props.title}</div>
         <div className="c_amount">{props.amount}$</div>
       </div>
